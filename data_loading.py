d98f3cdc9474927af2c6ce9e21cbaf2cd3a2c37d
kategr5 <57288974+kategr5@users.noreply.github.com>
2019-11-02 16:20:49 +0100

    Add files via upload

---
+++ data_loading.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+import string
+import torch
+from torch.utils.data import Dataset,DataLoader
+from sklearn.datasets import fetch_20newsgroups
+
+
+class AGnews(Dataset):
+    def __init__(self, dataset):
+        self.dataset=dataset
+        self.x_data=dataset[1]+dataset[2]
+        y_data=np.array(dataset[0])
+        for i in range(len(y_data)):
+            y_data[i] -= 1
+        self.y_data=y_data
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        #this is where the encoding happens
+        x_data=self.encoded(index)
+        y_data=self.y_data[index]
+        return x_data,y_data
+
+    def encoded(self,index):
+        alphabet = list(string.ascii_lowercase) + list(string.digits) + list(string.punctuation) + list('’') + list('\n')
+        encoded_data=torch.zeros(70,1014)
+        chars=self.x_data[index]
+        for index, char in enumerate(chars[::-1]):
+            if char in alphabet:
+                encoded_data[alphabet.index(char)][index]=1
+        return encoded_data
+
+class Newsground(Dataset):
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.x_data = dataset.filenames
+        self.y_data = dataset.targets
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        #this is where the encoding happens
+        x_data=self.encoded(index)
+        y_data=self.y_data[index]
+        return x_data,y_data
+
+    def encoded(self,index):
+        alphabet = list(string.ascii_lowercase) + list(string.digits) + list(string.punctuation) + list('’') + list('\n')
+        encoded_data=torch.zeros(70,1014)
+        chars=self.x_data[index]
+        for index, char in enumerate(chars[::-1]):
+            if char in alphabet:
+                encoded_data[alphabet.index(char)][index]=1
+        return encoded_data
+
+
+def load_data(dataset='<dataset>',transformation=None,n_train=None,n_test=None):
+
+
+    if dataset=='AGNews':
+        train = pd.read_csv('https://raw.githubusercontent.com/mhjabreel/CharCnn_Keras/master/data/ag_news_csv/train.csv',header=None)  # 120000x3
+        test = pd.read_csv('https://raw.githubusercontent.com/mhjabreel/CharCnn_Keras/master/data/ag_news_csv/test.csv',header=None)  # 7600x3
+        trainset = AGnews(train)
+        train_loader = DataLoader(dataset=trainset, batch_size=128, num_workers=0, drop_last=False)
+        testset = AGnews(test)
+        test_loader = DataLoader(dataset=testset, batch_size=128, num_workers=0, drop_last=False)
+
+        return train_loader, test_loader
+    elif dataset=='20Newsground':
+        train = fetch_20newsgroups(subset='train')
+        test = fetch_20newsgroups(subset='test')
+        trainset = Newsground(train)
+        train_loader = DataLoader(dataset=trainset, batch_size=128, num_workers=0, drop_last=False)
+        testset = Newsground(test)
+        test_loader = DataLoader(dataset=testset, batch_size=128, num_workers=0, drop_last=False)
+
+        return train_loader, test_loader
---
+++ run.py
@@ -0,0 +1,63 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from sys import stdin
+from model import Model
+from data_loading import load_data
+
+
+data=input('Type Dataset Choice, AGNews or 20Newsground:  ')
+
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print('device used: ',device)
+
+model = Model()
+model.to(device)
+
+#define train/test here
+train_loader,test_loader=load_data(dataset=data)
+
+loss_crit=nn.CrossEntropyLoss()
+#optimizer=optim.SGD(model.parameters(),lr=0.001,momentum=0.9)
+optimizer=optim.Adam(model.parameters(),lr=0.0001)
+
+print("Starting training...")
+for epoch in range(5):
+
+    running_loss=0.0
+
+    for i,data in enumerate(train_loader,0):
+        inputs,labels=data
+        inputs,labels=torch.tensor(inputs),torch.tensor(labels)
+        inputs, labels = inputs.to(device), labels.to(device)
+        optimizer.zero_grad()
+        y_pred=model(inputs)
+        #print(labels.shape,y_pred.shape)
+        loss = loss_crit(y_pred, labels.long())
+        #print(epoch, loss.data.item())
+
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if i % 10 == 9:  # print every 10 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 10))
+            running_loss = 0.0
+
+print('Finished training')
+print('Starting testing')
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in test_loader:
+        inputs, labels = data
+        inputs, labels = torch.tensor(inputs), torch.tensor(labels)
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels.long()).sum().item()
+print('Accuracy of the network on the test inputs: %d %%' % (100 * correct / total))
